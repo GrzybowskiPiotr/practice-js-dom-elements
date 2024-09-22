15dff70de13040adce4f6c032fc8c8efb463a7b2
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-22 15:43:46 +0200

    chalange complete

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,35 @@
-console.log('DOM');
+console.log("DOM");
+const nav = document.querySelector("nav");
+const ul = document.createElement("ul");
+
+function createLiA(href, text) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.setAttribute("href", href);
+  a.innerText = text;
+  li.appendChild(a);
+  return li;
+}
+
+ul.appendChild(createLiA("/", "start"));
+ul.appendChild(createLiA("/galeria", "galeria"));
+ul.appendChild(createLiA("/kontakt", "kontakt"));
+nav.appendChild(ul);
 
 // struktura do wykorzystania w pętli
+
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: "start", url: "/" },
+  { text: "galeria", url: "/gallery" },
+  { text: "kontakt", url: "/contact" },
 ];
+
+function createMenuItems(items) {
+  const ul = document.createElement("ul");
+  items.forEach((element) => {
+    ul.appendChild(createLiA(element.url, element.text));
+  });
+  return ul;
+}
+
+nav.appendChild(createMenuItems(menuItems));
