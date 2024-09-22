0cf60b856d32c1a680a62f532c89d5360a6d486e
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-22 08:38:59 +0200

    chalange completed

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,16 @@
-console.log('DOM');
+const commnetsItems = document.querySelectorAll(
+  ".comments__item.comments__item--newest"
+);
+
+let itemsCount = 0;
+
+commnetsItems.forEach((element) => {
+  const itemsWithDataInfo = element.querySelectorAll("[data-info]");
+  itemsCount += itemsWithDataInfo.length;
+});
+
+console.log(`Odnaleziono ${itemsCount} elementów w atrybutem [data-info]`);
+
+const dataItems = document.querySelector(
+  ".comments__item.comments__item--newest"
+);
